c2c370bf2456d163182816f7a70330f6dc9be525
Ted Vu <tedvu184@gmail.com>
2021-06-07 21:32:12 +1000

    Spin up an express server

---
+++ callback.js
@@ -0,0 +1,8 @@
+const posts = [
+  { title: "Post One", body: "This is a post one" },
+  { title: "Post Two", body: "This is a post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {}, 1000);
+}
