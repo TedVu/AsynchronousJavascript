0203d222a64cd66cffdd65af44b80dc2e84733c7
Ted Vu <tedvu184@gmail.com>
2021-06-28 22:10:48 +1000

    Exploring double then syntax

--- client/promise_2.js
+++ client/promise_2.js
@@ -1,36 +1,21 @@
-const posts = [
-  { title: "Post One", body: "This is a post one" },
-  { title: "Post Two", body: "This is a post two" },
-];
+const fetch = require("node-fetch");
+function displayData(array) {
+  let list = "<ul>";
 
-// const fetch = require("node-fetch");
-
-function getPosts() {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-}
-
-function createPost(post) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(post);
-      const error = true;
-      if (!error) {
-        resolve();
-      } else {
-        reject("Error: Something went wrong");
-      }
-    }, 2000);
+  array.forEach((element) => {
+    list += `<li>${element}</li>`;
   });
-}
 
-const promise1 = Promise.resolve("Hello World");
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, "Goodbye");
-});
+  list += "</ul>";
+  document.body.innerHTML = list;
+}
+alert("hello world");
+
+// why double then ?
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((item) => {
+    const todos = [];
+    todos.push(item.title);
+    displayData(todos);
+  });
